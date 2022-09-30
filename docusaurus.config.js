8574991559b31ad34c4308ca2e7be52709fffae9
Detroit Whiskey <detroitwhiskey136@gmail.com>
2022-09-29 17:14:17 -0700

    Change the Baseurl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'DBM Mods Documentation',
   tagline: 'For all your dbm mod development needs.',
   url: 'https://docs.dbm-network.org/',
-  baseUrl: '/documentation/',
+  baseUrl: '/',
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
